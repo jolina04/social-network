de0be48788eb4313ef0c2ebf5bc8f885c5b815c4
Jolina <coronelmarijolina@gmail.com>
2019-02-27 17:49:06 +0800

    Added modal for editing post.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,4 +39,23 @@
             @endforeach
         </div>
     </section>
+    <div class="modal" tabindex="-1" role="dialog" id="edit-modal">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Edit Post</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
